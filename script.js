e116a00e5674eb3ba5eeb126b130742550f81a13
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-04 18:05:35 +0200

    milestone 2 done

--- script.js
+++ script.js
@@ -2,23 +2,48 @@
 //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-const display = document.getElementById('panel');
+const panel = document.getElementById('panel');
+const display = document.getElementById('display');
 
 //Genero un array di 5 numeri casuali diversi tra di loro
 const arrayNumbers = [];
 
 function getRandomNumber (min = 1, max = 100){
-    return Math.floor(Math.random() * (max + 1 - min)) + min;
+    const randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return randomNumber
 }
 
-for(let i = 0; i < 5; i++){
-    let randomNumbers = getRandomNumber(1,100);
 
-    while(arrayNumbers.includes(randomNumbers)){
-        randomNumbers = getRandomNumber(1,100);
+while(arrayNumbers.length < 5){
+    let myNumbers = getRandomNumber(1,100);
+    if(!arrayNumbers.includes(myNumbers)){
+        arrayNumbers.push(myNumbers)
     }
-    arrayNumbers.push(randomNumbers);
+    
 }
+panel.innerHTML = arrayNumbers;
 
-getRandomNumber(min, max);
-console.log(randomNumbers)
+//Faccio startare il countdown a partire da 10 secondi,lo blocco a 0 e faccio sparire i numeri generati
+ let seconds = 10;
+ display.innerText = seconds;
+
+ const countdown = setInterval( function(){
+    display.innerText = --seconds;
+    
+    if(seconds === 0){
+        clearInterval(countdown);
+        display.innerText = 'Riscrivi ora la lista di numeri in ordine'
+        panel.innerText = '';
+    }
+
+ },1000);
+
+
+ //Chiedo all'utente di inserire i numeri in ordine
+setTimeout( function(){
+    const firstUserNumber = prompt('Inserisci il primo numero della lista');
+    const secUserNumber = prompt('Inserisci il secondo numero della lista');
+    const thirdUserNumber = prompt('Inserisci il terzo numero della lista');
+    const fourthtUserNumber = prompt('Inserisci il quarto numero della lista');
+    const fifthUserNumber = prompt('Inserisci il quinto numero della lista');
+},12000);
